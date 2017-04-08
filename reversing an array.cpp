a5a08bd6d10be578fe45634b7c56f1bc56dd41ef
Dinesh Kumar <dineshchunduspecial@gmail.com>
2017-04-08 23:50:20 +0530

    Update reversing an array.cpp

--- reversing an array.cpp
+++ reversing an array.cpp
@@ -24,3 +24,18 @@ int main()
     }
     return 0;
 }
+/* can also be done like this
+#include<bits/stdc++.h> //this header file contains all the header files of c++ if u include this
+//no need to include any other but this takes a bit more time to execute so use 
+#include<iostream>
+#include<algorithm>
+typedef int ma;
+int main(){
+  ma n;
+  cin>>n;
+  ma arr[n];
+  for(ma i=0;i<n;i++) cin>>arr[i];
+  reverse(arr,arr+n);
+  for(ma i=0;i<n;i++) cout<<arr[i];
+}
+*/
